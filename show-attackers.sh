3492947444ba0351ef6c2e4d485db4e292ce2542
mozo95 <57413382+mozo95@users.noreply.github.com>
2019-12-10 20:47:05 +0100

    Update show-attackers.sh

--- show-attackers.sh
+++ show-attackers.sh
@@ -25,13 +25,26 @@ then
 fi
 # Display the CSV header.
 echo "Intentos          IP          Localizacion"
-# Loop through the list of failed attempts and corresponding IP addresses.
-cat "$1" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq -c | sort -nr | while read TRYS IP
+#Declare Array
+declare -A array
+#Parse file into variable
+fichero=$(cat "$1" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq -c) 
+#Internal Field Separator spliting only in \n
+IFS=$'\n'
+#Populate the array
+for linia in $fichero
 do
-# If the number of failed attempts is greater than the limit, display count, IP, and location.
-	if [[ $TRYS -gt "$MAX" ]]
-	then
-		location=$(geoiplookup "$IP" | cut -d ' ' -f4,5) 
-		echo "$TRYS          $IP          $location"
-	fi
+	TRY=$(echo "$linia" | awk '{print $2}')
+        IP=$(echo "$linia" | awk '{print $1}')
+	array["$IP"]="$TRY"
 done
+#Main function
+for i in "${!array[@]}"
+do
+        # If the number of failed attempts is greater than the limit, display count, IP, and location
+	if [[ "$i" -gt "$MAX" ]]
+        then
+                location=$(geoiplookup "${array[$i]}" | cut -d ' ' -f4,5)
+                echo "$i          ${array[$i]}          $location"
+        fi
+done | sort -nr -k1
